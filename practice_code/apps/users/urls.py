a8a3dd10cb54a069bc5d91e94d993cc04c08a454
tima <tima62507@gmail.com>
2025-09-09 13:58:18 +0600

    Add files via upload

--- practice_code/apps/users/urls.py
+++ practice_code/apps/users/urls.py
@@ -1,15 +1,52 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+"""
+URL configuration for core project.
 
-from apps.users.views import RegisterView, LoginView, ProfileView, UserViewSet
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-router = DefaultRouter()
-router.register(r"users", UserViewSet, basename="users")
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'library 28-1B',
+        default_version = 'v1',
+        description = 'library 28-1B',
+        terms_of_service = 'https://www.google.com/policies/terms/',
+        contact = openapi.Contact(email='tima62507@gmail.com'),
+        license = openapi.License(name='BSD License'),
+    ),
+    public = True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/<str:token>/', ProfileView.as_view(), name='profile'),
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/v1/settings/', include("apps.settings.urls")),
+    path('api/v1/users/', include("apps.users.urls")),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
